bcaf381d368bfa2ef9d7a906741ea2e74d9dda3b
family <214151436@qq.com>
2020-10-27 18:04:35 +0800

    'getters,mutations,actions,module'

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,115 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const moduleA = {
+  state: () => ({
+    name: '张三'
+  }),
+  mutations: {
+    updateName(state,payload) {
+      state.name = payload
+    }
+  },
+  actions: {},
+  getters: {
+    fullName(state) {
+      return '真·' + state.name;
+    },
+    // 这里传入的是整个getter对象，不止是模块中可用，根store的getters中也可以这么定义函数
+    fullName2(state,getter){
+      return '真~' + getter.fullName;
+    }
+  }
+}
 
 const store = new Vuex.Store({
-    state: {
-        counter: 0
-    },
-    mutations: {
-        //方法
-        increment(state){
-            state.counter++;
-        },
-        decrement(state){
-            state.counter--;
-        }
+  state: {
+    counter: 0,
+    students: [
+      {
+        name: 'a',
+        age: 18
+      },
+      {
+        name: 'b',
+        age: 20
+      },
+      {
+        name: 'c',
+        age: 22
+      },
+      {
+        name: 'd',
+        age: 24
+      }
+    ],
+    info: {
+      name: 'kobe',
+      age: 18,
+      height: 1.98
     }
+  },
+  mutations: {
+    // 同步方法 只能通过commit方法来更新store的状态
+    // 这里定义的函数可以看做两个部分
+    // 字符串的事件类型(type)
+    // 一个回调函数(handler)，该回调函数的第一个参数就是state
+    increment(state){
+      state.counter++;
+    },
+    decrement(state){
+      state.counter--;
+    },
+    incrementCount(state,count) {
+      state.counter += count;
+    },
+    updateInfo(state){
+      state.info.name = 'coder';
+      // 这样的修改不能做到响应式
+      // state.info['address'] = 'LosAngeles';
+      // 这种修改能做到响应式
+      // Vue.set(state.info,'address','LosAngeles')
+      // 删除对象中的某一个属性，但是在Vuex中不能做到响应式
+      // delete state.info.name;
+      // 这种做法能够做到响应式
+      // Vue.delete(state.info,'name')
+    }
+  },
+  // 类似于单个组件中定义的computed方法
+  getters: {
+    powerCounter(state) {
+      return state.counter * state.counter;
+    },
+    olderThan20(state){
+      return state.students.filter(e => e.age > 20);
+    },
+    // 如果调用方法想带有自定义传入参数的时候可以使用这种写法
+    // 可以传递一个参数或者多个参数
+    // 这里代表的是return一个函数，传入的参数会带入到返回的函数中去执行
+    olderThanAge(state) {
+      return age => state.students.filter(e => e.age >age);
+    }
+
+  },
+  actions: {
+    // 异步方法，方法中的默认提供参数是上下文context
+    // asyncUpdateInfo(context, payload) {
+    //   setTimeout(() => {
+    //     context.commit('updateInfo');
+    //     console.log(payload);
+    //   },1000)
+    // }
+    async aUpdateInfo(context,payload){
+      await context.commit('updateInfo');
+      console.log(payload);
+    }
+  },
+  modules: {
+    a: moduleA
+  }
+
 })
 
-export default store
+export default store;
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
     <h2>首页</h2>
+    <h2>--------App内容：modules的内容--------</h2>
+    <h2>{{$store.state.a}}</h2>
+    <h2>{{$store.state.a.name}}</h2>
+    <h2>{{$store.getters.fullName}}</h2>
+    <h2>{{$store.getters.fullName2}}</h2>
+    <button @click="changeModuleState('李四')">修改module中的name状态</button>
+
+    <h2>--------App内容：info对象的内容是否是响应式--------</h2>
+    <h2>{{$store.state.info}}</h2>
+    <button @click="updateInfo">修改信息</button>
     <h1>这里是父组件的共享状态</h1>
     <h2>{{$store.state.counter}}</h2>
     <button @click="addition">+</button>
     <button @click="subtraction">-</button>
+    <button @click="additionCount(5)">+5</button>
+    <button @click="additionCount(10)">+10</button>
+    <h1>getters 相关内容</h1>
+    <p>{{$store.getters.powerCounter}}</p>
+    <p>{{$store.getters.olderThan20}}</p>
+    <p>{{$store.getters.olderThanAge(18)}}</p>
+
     <h1>这里是子组件中的共享状态</h1>
     <HelloVuex></HelloVuex>
   </div>
@@ -24,6 +41,26 @@ export default {
     },
     subtraction() {
       this.$store.commit('decrement');
+    },
+    additionCount(count) {
+      // 普通的提交风格
+      // this.$store.commit('incrementCount',count)
+      // 特殊的提交风格
+      this.$store.commit({
+        type: 'incrementCount',
+        count: count
+      });
+    },
+    updateInfo(){
+      // this.$store.commit('updateInfo');
+      // this.$store.dispatch('asyncUpdateInfo','我是payload');
+      this.$store.dispatch('aUpdateInfo','我是payload')
+          .then(() => {
+            console.log('提交完成可以调用别的了');
+          });
+    },
+    changeModuleState(newName){
+      this.$store.commit('updateName',newName);
     }
   }
 }
