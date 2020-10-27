bbf34c5ae74e86ce87101b4c8eeb8a1703769659
SirGaga <214151436@qq.com>
2020-10-27 21:31:07 +0800

    'axios'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,23 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+const config = {
+  url:'http://123.207.32.32:8000/home/multidata',
+}
+
+axios(config).then(res => {
+  console.log(res);
+});
+
+const configParam = {
+  url: 'http://123.207.32.32:8000/home/data',
+  params: {
+    type: 'pop',
+    page: 1
+  }
+}
+
+axios(configParam).then(res => {
+  console.log(res);
+})
